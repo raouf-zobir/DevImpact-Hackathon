17b5d7a73c174e3fd40fba10e187006bde1a712e
AnasGT <anasalasmar10@gmail.com>
2025-01-10 23:49:08 +0100

    add apsense

--- code/frontend/lib/core/constants/app_colors.dart
+++ code/frontend/lib/core/constants/app_colors.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryPurple = Color(0xFF4D44B5);
+  static const primaryPurple = Color.fromARGB(255, 196, 68, 68);
   static const accentOrange = Color(0xFFFB7D5B);
   static const backgroundWhite = Color(0xFFF5F5F5);
   static const textBlack = Color(0xFF303972);
   static const secondaryGreen = Color(0xFF4CBC9A);
   static const errorRed = Color(0xFFFF4550);
   static const borderGray = Color(0xFFDBDBDB);
-  static const buttonBlue = Color(0xFF428777);
+  static const buttonBlue = Color(0xFF4A6BDB); // Changed from dark green to a pleasant blue
   static const highlightYellow = Color(0xFFFCC43E);
   static const subtleGray = Color(0xFF363B64);
   static const lightPurple = Color(0xFFF3F4FF);
--- code/frontend/lib/view/drawer/widgets/drawer_list_item.dart
+++ code/frontend/lib/view/drawer/widgets/drawer_list_item.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sama/controller/drawer_controller.dart';
+import 'package:sama/controller/navigations_controller.dart';
+import 'package:sama/core/constants/app_colors.dart';
 import 'package:sama/model/drawer_item_model.dart';
 import 'package:sama/view/drawer/widgets/drawer_item.dart';
+import 'package:sama/core/enum/navigations_enum.dart';
 
 class DrawerListItem extends StatelessWidget {
   const DrawerListItem({
@@ -11,24 +14,28 @@ class DrawerListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ...List.generate(
-            drawerItem.length,
-            (index) => Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: GetBuilder<DrawerControllerImp>(builder: (controller) {
-                    return GestureDetector(
-                      onTap: () => controller.toggleItemDrawer(index, drawerItem[index].destination),
-                      child: DrawerItem(
-                        drawerItemModel: drawerItem[index],
-                        isActive: controller.drawerItemIsActive == index,
-                      ),
-                    );
-                  }),
-                ))
-      ],
+    return GetBuilder<DrawerControllerImp>(
+      builder: (controller) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(
+          drawerItem.length,
+          (index) => Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: GetBuilder<DrawerControllerImp>(
+              builder: (controller) => GestureDetector(
+                onTap: () => controller.toggleItemDrawer(
+                  index,
+                  drawerItem[index].destination,
+                ),
+                child: DrawerItem(
+                  drawerItemModel: drawerItem[index],
+                  isActive: controller.drawerItemIsActive == index,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
