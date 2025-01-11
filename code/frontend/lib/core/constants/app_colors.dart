fb952aa76522b5c8fc167f66ddaad473d6876110
hanini-2024 <zobiraouf@gmail.com>
2025-01-11 02:13:47 +0100

    New Update

--- code/frontend/lib/core/constants/app_colors.dart
+++ code/frontend/lib/core/constants/app_colors.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryPurple = Color.fromARGB(255, 196, 68, 68);
+  static const primaryPurple = Color(0xFF6200EA); // Deep purple
   static const accentOrange = Color(0xFFFB7D5B);
   static const backgroundWhite = Color(0xFFF5F5F5);
   static const textBlack = Color(0xFF303972);
   static const secondaryGreen = Color(0xFF4CBC9A);
   static const errorRed = Color(0xFFFF4550);
   static const borderGray = Color(0xFFDBDBDB);
-  static const buttonBlue = Color(0xFF4A6BDB); // Changed from dark green to a pleasant blue
+  static const buttonBlue =
+      Color(0xFF4A6BDB); // Changed from dark green to a pleasant blue
   static const highlightYellow = Color(0xFFFCC43E);
   static const subtleGray = Color(0xFF363B64);
   static const lightPurple = Color(0xFFF3F4FF);
